d8b5e11acb4a221d9038ef468dd4534a43c05c62
lukachanishvil <lukachanishvili43@gmail.com>
2024-12-31 01:45:18 +0400

    Add files via upload

---
+++ lesson 04/classwork/task2.py
@@ -0,0 +1,18 @@
+str1 = "Hello"
+str2 = "World"
+
+
+int1 = 5
+int2 = 10
+
+
+float1 = 3.14
+float2 = 2.71
+
+
+print(str1)
+print(str2)
+print(int1)
+print(int2)
+print(float1)
+print(float2)
---
+++ lesson 04/homework/task9.py
@@ -0,0 +1,17 @@
+# ოჯახური წევრების ასაკების ცვლადები
+father_age = 45
+mother_age = 42
+brother_age = 18
+sister_age = 15
+
+# 20 წლის შემდეგ ასაკების განახლება
+father_age_in_20_years = father_age + 20
+mother_age_in_20_years = mother_age + 20
+brother_age_in_20_years = brother_age + 20
+sister_age_in_20_years = sister_age + 20
+
+# შედეგების გამოქვეყნება
+print(f"მამა 20 წლის შემდეგ იქნება {father_age_in_20_years} წლის.")
+print(f"დედა 20 წლის შემდეგ იქნება {mother_age_in_20_years} წლის.")
+print(f"ძმა 20 წლის შემდეგ იქნება {brother_age_in_20_years} წლის.")
+print(f"დაც 20 წლის შემდეგ იქნება {sister_age_in_20_years} წლის.")
